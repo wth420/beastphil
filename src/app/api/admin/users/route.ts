2fa8a11d334eb7e79465b444158e602249af3c50
Mofe Odujirin <thebaggieboy@protonmail.com>
2026-04-06 07:37:43 -0400

    second commit

--- src/app/api/admin/users/route.ts
+++ src/app/api/admin/users/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: Request) {
   try {
     // For this demo, we allow all GET requests to this admin endpoint.
